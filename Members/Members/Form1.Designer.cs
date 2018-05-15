22fb00c53749da8a032895ed1e578aebaa70b06e
refelia <rherlihy20@gmail.com>
2018-05-15 12:24:57 -0400

    Adding members file -INCOMPLETE

---
+++ Members/Members/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Members
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.memSortDatagrid = new System.Windows.Forms.DataGridView();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.exportBtn = new System.Windows.Forms.Button();
+            this.reformatDateBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.memSortDatagrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // memSortDatagrid
+            // 
+            this.memSortDatagrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.memSortDatagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.memSortDatagrid.Location = new System.Drawing.Point(12, 21);
+            this.memSortDatagrid.MultiSelect = false;
+            this.memSortDatagrid.Name = "memSortDatagrid";
+            this.memSortDatagrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.memSortDatagrid.Size = new System.Drawing.Size(715, 301);
+            this.memSortDatagrid.TabIndex = 0;
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(64, 340);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(75, 23);
+            this.loadBtn.TabIndex = 1;
+            this.loadBtn.Text = "Load";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // exportBtn
+            // 
+            this.exportBtn.Location = new System.Drawing.Point(469, 340);
+            this.exportBtn.Name = "exportBtn";
+            this.exportBtn.Size = new System.Drawing.Size(75, 23);
+            this.exportBtn.TabIndex = 2;
+            this.exportBtn.Text = "Export";
+            this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Click += new System.EventHandler(this.exportBtn_Click);
+            // 
+            // reformatDateBtn
+            // 
+            this.reformatDateBtn.Location = new System.Drawing.Point(260, 340);
+            this.reformatDateBtn.Name = "reformatDateBtn";
+            this.reformatDateBtn.Size = new System.Drawing.Size(104, 23);
+            this.reformatDateBtn.TabIndex = 3;
+            this.reformatDateBtn.Text = "Reformat the Date";
+            this.reformatDateBtn.UseVisualStyleBackColor = true;
+            this.reformatDateBtn.Click += new System.EventHandler(this.reformatDateBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 375);
+            this.Controls.Add(this.reformatDateBtn);
+            this.Controls.Add(this.exportBtn);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.memSortDatagrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.memSortDatagrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView memSortDatagrid;
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.Button exportBtn;
+        private System.Windows.Forms.Button reformatDateBtn;
+    }
+}
+
---
+++ Members/Members/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Members
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            string delimiter = ",";
+            string tablename = "memList";
+            DataSet dataset = new DataSet();
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                StreamReader sr = new StreamReader(filename);
+                string csv = File.ReadAllText(openFileDialog1.FileName);
+
+                dataset.Tables.Add(tablename);
+                dataset.Tables[tablename].Columns.Add("FirstName");
+                dataset.Tables[tablename].Columns.Add("LastName");
+                dataset.Tables[tablename].Columns.Add("BirthDate");
+                dataset.Tables[tablename].Columns.Add("City");
+                dataset.Tables[tablename].Columns.Add("State");
+                dataset.Tables[tablename].Columns.Add("DateJoin");
+
+
+                string allData = sr.ReadToEnd();
+                string[] rows = allData.Split("\r".ToCharArray());
+
+
+                foreach (string r in rows)
+                {
+                    string[] items = r.Split(delimiter.ToCharArray());
+                    dataset.Tables[tablename].Rows.Add(items);
+                }
+
+                //Display the result to the datagrid.
+                foreach (string value in rows)
+                {
+                    memSortDatagrid.DataSource = dataset.Tables[0];
+                }
+            }
+        }
+
+        private void reformatDateBtn_Click(object sender, EventArgs e)
+        {
+            //memSortDatagrid.Rows.Clear();
+           
+           
+        }
+
+        private void exportBtn_Click(object sender, EventArgs e)
+        {
+            string pathFinder = "members.csv";
+            StreamWriter stwtr = new StreamWriter(pathFinder);
+
+            foreach (string listOfMembers in memSortDatagrid.DataBindings)
+            {
+                //write a text file.
+               stwtr.WriteLine(listOfMembers.ToString());
+
+                //Display message
+                MessageBox.Show("Exported!");
+            }
+        }
+    }
+}
+
+
+
